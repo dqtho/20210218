ee2011b5233a3bf237b4363e2ed86bec13184982
villageworks-tutor <villageworks-tutor@gmail.com>
2021-02-16 21:51:03 +0900

    2021-02-17

---
+++ oop/task/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8" />
+	<title>課題</title>
+</head>
+<body>
+	<h1>ログイン</h1>
+	<p>ユーザIDとパスワードを入力して、［ログイン］ボタンをクリックしてください。</p>
+	<table>
+	<form action="" method="">
+		<tr>
+			<th>ユーザID</th>
+			<td><input type="text" name="id" /></td>
+		</tr>
+		<tr>
+			<th>パスワード</th>
+			<td><input type="text" name="password" /></td>
+		</tr>
+		<tr>
+			<td colspan="2"><input type="submit" value="ログイン" /></td>
+		</tr>
+	</form>
+	</table>
+</body>
+</html>
